1b8e1a2a9299861a7266e4c884b17cf820b750a9
ChrisKLoveless <chriskloveless@gmail.com>
2022-11-16 10:02:29 -0800

    add async function and link to index.js
    co-authorred-by: brandon fenk <brandon.fenk99@gmail.com>
    co-authored-by: Bodie Wood <bodiewood@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './css/styles.css';
+import brewApi from './js/brew-api.js';
+
+function getBrewery(city, state) {
+	let promise = brewApi.getBrewery(city, state);
+	promise.then(function (breweryArray) {
+		printElements(breweryArray);
+	}, function (errrorray) {
+		printError(errorArray);
+	});
+}
+
---
+++ src/js/brew-api.js
@@ -0,0 +1,18 @@
+export default class brewApi {
+	static getBrewery(city, state) {  //city and state parameters are for the string literal below i.e. URL
+		return new Promise(function (resolve, reject) {
+			let request = new XMLHttpRequest();  //new request object
+			const url = `https://api.openbrewerydb.org/breweries?by_city=${city}&by_state=${state}&per_page=10`;
+			request.addEventListener("loadend", function () {
+				const response = JSON.parse(this.responseText);
+				if (this.status == 200) {
+					resolve([response, city, state]);  //this keyword is referencing the request XHR
+				} else {
+					reject([this, response, city, state]);
+				}
+			});
+			request.open("GET", url, true);
+			request.send();
+		});
+	}
+}
