3f9c85af5d707a0ef0bbb9d132052d7feae9ebd2
ChrisKLoveless <chriskloveless@gmail.com>
2022-11-16 16:47:15 -0800

    add styling to page and images
    co-authorred-by: brandon fenk <brandon.fenk99@gmail.com>
    co-authored-by: Bodie Wood <bodiewood@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import brewApi from './js/brew-api.js';
 
-function getBrewery(city, state) {
-	let promise = brewApi.getBrewery(city, state);
+function getBrewery(city, state, type) {
+	let promise = brewApi.getBrewery(city, state, type);
 	promise.then(function (breweryArray) {
 		printElements(breweryArray);
 	}, function (errorArray) {
@@ -19,9 +19,12 @@ function printElements(data) {
 	document.getElementById("output").innerText = `Here are some breweries in ${data[1]}`;
 
 	dataArray.forEach(function (id) {
-		let p = document.createElement("p");
-		p.append(id.name, id.street, id.phone);
-		document.getElementById("output").append(p);
+		let ul = document.createElement("ul");
+		let li = document.createElement("li");
+		const infoString = `${id.name} | located: ${id.street} | call at: ${id.phone}`;
+		li.append(infoString);
+		ul.append(li);
+		document.getElementById("output").append(ul);
 	});
 }
 
@@ -34,12 +37,14 @@ function handleSubmit(event) {
 
 	const city = document.getElementById("city-input").value;
 	const state = document.getElementById("state-input").value;
+	const type = document.getElementById("brewType").value;
+	document.getElementById("brewType").value = null;
 	document.getElementById("city-input").value = null;
 	document.getElementById("state-input").value = null;
 
-	getBrewery(city, state);
+	getBrewery(city, state, type);
 }
 
 window.addEventListener("load", function () {
 	document.querySelector("form").addEventListener("submit", handleSubmit);
-})
+});
--- src/js/brew-api.js
+++ src/js/brew-api.js
@@ -1,14 +1,14 @@
 export default class brewApi {
-	static getBrewery(city, state) {  //city and state parameters are for the string literal below i.e. URL
+	static getBrewery(city, state, type) {  //city and state parameters are for the string literal below i.e. URL
 		return new Promise(function (resolve, reject) {
 			let request = new XMLHttpRequest();  //new request object
-			const url = `https://api.openbrewerydb.org/breweries?by_city=${city}&by_state=${state}&per_page=10`;
+			const url = `https://api.openbrewerydb.org/breweries?by_city=${city}&by_state=${state}&by_type=${type}&per_page=10`;
 			request.addEventListener("loadend", function () {
 				const response = JSON.parse(this.responseText);
 				if (this.status == 200) {
-					resolve([response, city, state]);  //this keyword is referencing the request XHR
+					resolve([response, city, state, type]);  //this keyword is referencing the request XHR
 				} else {
-					reject([this, response, city, state]);
+					reject([this, response, city, state, type]);
 				}
 			});
 			request.open("GET", url, true);
